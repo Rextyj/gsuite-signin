21981eecb0533819c3fcbb813ecda62520eed26e
Yijie Tong <rex.tyj@gmail.com>
2019-12-18 16:40:42 -0500

    Some testing

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -26,18 +26,37 @@ export class HomePage {
         this.auth2 = gapi.auth2.init({
           client_id:
             "852792885400-v960fh3akkt9f07pd02u2fagfjdbmi93.apps.googleusercontent.com"
+            // '852792885400-akmbv2bbm2k9dnshgd061gh6bah9qddh.apps.googleusercontent.com'
+
         });
 
 
-        this.auth2.then((val) => {
-          console.log('returned obj ', val)
-          let result = this.auth2.isSignedIn.get();
-          console.log("result is ", result);
-        });
+        // this.auth2.then((val) => {
+        //   console.log('returned obj ', val)
+        //   let result = this.auth2.isSignedIn.get();
+        //   console.log("result is ", result);
+        // });
+        
 
-        this.auth2.signIn().then((val) => {
-          console.log('signin result is ', val);
-        })
+        // let signInOptions = new gapi.auth2.SigninOptionsBuilder();
+        // signInOptions.setScope('https://www.googleapis.com/auth/calendar');
+        // console.log('sigin in options ', signInOptions);
+        // this.auth2.signIn().then((val) => {
+        //   console.log('signin result is ', val);
+          
+        //   // if(this.auth2.isSignedIn.get()) {
+        //   //  let user = this.auth2.currentUser.get();
+        //   //  console.log('current user ', user);
+        //   //  let authResponse = user.getAuthResponse(true);
+        //   //  console.log('auth response ', authResponse);
+        //   // }
+        // })
+
+        this.auth2.grantOfflineAccess().then((code)=> {
+            console.log('offline code ', code);
+          }).catch((err) => {
+            console.log('error is ', err);
+          })
       });
     });
   }
